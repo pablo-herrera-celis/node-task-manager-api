0056ceae8a286ac9f1b50992e6d36f099e3def93
pablo-herrera-celis <pablo.herrera.celis@unillanos.edu.co>
2023-07-28 13:35:17 -0500

    File Uploads

--- src/index.js
+++ src/index.js
@@ -6,17 +6,15 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-// app.use((req, res, next) => {
-//     if (req.method === 'GET') {
-//         res.send('GET request are disabled')
-//     } else {
-//         next()
-//     }
-// })
 
-// app.use((req, res, next) => {
-//     res.status(503).send('Maintenance in process. Check back soon!')
-// })
+const multer = require('multer')
+const upload = multer({
+    dest: 'images'
+})
+app.post('/upload', upload.single('upload'), (req, res) => {
+    res.send()
+})
+
 
 app.use(express.json())
 app.use(userRouter)
@@ -25,18 +23,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
-
-const Task = require('./models/task')
-const User = require('./models/user')
-
-const main = async () => {
-    // const task = await Task.findById('649b26106572af04d5420182')
-    // await task.populate('owner')
-    // console.log(task.owner)
-
-    // const user = await User.findById('649b363086d94e23c177d72d')
-    // await user.populate('tasks')
-    // console.log(user.tasks)
-}
-
-main()
